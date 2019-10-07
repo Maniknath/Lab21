93a17b955eb1d4ea57c990097bd1f772efb328b3
maniknath <maniknath79@gmail.co>
2019-10-07 18:06:09 -0400

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstMVCApp.Models
+{
+    public class User
+    {
+        //[Key]
+        //[Required]
+        //[StringLength(10, MinnimumLength=3)]
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+
+        //[MinimumLength(3)]
+        //[MaxLength(10)]
+        //[Required]
+        public string Password { get; set; }
+
+        // [Required]
+
+
+
+    }
+
+}
